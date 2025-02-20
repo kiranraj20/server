30c543371468a41db27163a36f6959233bb115de
kiranraj20 <kiranraj80555@gmail.com>
2025-02-20 12:56:46 +0530

    .

--- routes/user-auth.js
+++ routes/user-auth.js
@@ -23,9 +23,7 @@ router.post("/create-user", async (req, res) => {
         }
 
         // Check if user already exists
-        const existingUser = await User.findOne({
-            $or: [{ email }, { firebaseUid: verifiedUid }],
-        });
+        const existingUser = await User.findOne({ email });
 
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
